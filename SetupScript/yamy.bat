8f564b0335fe1327438f8ed4c68889ee1e096c48
matsumoto_tatsuhei <matsumoto_tatsuhei@intec.co.jp>
2020-02-27 14:46:47 +0900

    wip yamy auto setup.

--- SetupScript/yamy.bat
+++ SetupScript/yamy.bat
@@ -1,11 +1,22 @@
 @echo off
-echo yamyのセットアップ
+
+echo --------------------------------
+echo yamyセットアップスクリプト
+echo --------------------------------
 
 set BIN_FOLDER=%USERPROFILE%\bin
 set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set "DOWNLOAD_URL=https://osdn.net/frs/redir.php?m=jaist&f=yamy%2F43637%2Fyamy-0.03.zip"
+set DOWNLOAD_PATH=%USERPROFILE%\bin\yamy
+set INSTALL_PATH=%USERPROFILE%\bin\yamy
 
-echo yamyをスタートアップに登録する
-cmd /c mksc "%STARTUP_FOLDER%\yamy.lnk" "%BIN_FOLDER%\yamy\yamy.exe"
+echo %DOWNLOAD_URL% %DOWNLOAD_PATH%
+bitsadmin.exe /TRANSFER xyzzy-download "%DOWNLOAD_URL%" "%DOWNLOAD_PATH%"
+
+
+
+rem echo yamyをスタートアップに登録する
+rem cmd /c mksc "%STARTUP_FOLDER%\yamy.lnk" "%BIN_FOLDER%\yamy\yamy.exe"
 
 pause
 
