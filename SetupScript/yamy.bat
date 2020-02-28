631b03bf3f162656b71e6a5b780a00a3388736d1
matsumoto_tatsuhei <matsumoto_tatsuhei@intec.co.jp>
2020-02-28 10:58:24 +0900

    update yamy setting script.

--- SetupScript/yamy.bat
+++ SetupScript/yamy.bat
@@ -1,22 +1,41 @@
 @echo off
 
 echo --------------------------------
-echo yamyセットアップスクリプト
+echo  yamyセットアップスクリプト
 echo --------------------------------
 
-set BIN_FOLDER=%USERPROFILE%\bin
-set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+rem --------------------------------
+rem  基本設定
+rem
+set BIN=%USERPROFILE%\bin
+set INSTALL_FALDER=%USERPROFILE%\bin\yamy
 set "DOWNLOAD_URL=https://osdn.net/frs/redir.php?m=jaist&f=yamy%2F43637%2Fyamy-0.03.zip"
-set DOWNLOAD_PATH=%USERPROFILE%\bin\yamy
-set INSTALL_PATH=%USERPROFILE%\bin\yamy
+set DOWNLOAD_PATH=%USERPROFILE%\bin\yamy\yamy.zip
+set EXE_PATH=%USERPROFILE%\bin\yamy\yamy.exe
+set LAUNCHAR_PATH=%USERPROFILE%\bin\yamy.lnk
+set "STARTUP_PATH=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\yamy.lnk"
 
-echo %DOWNLOAD_URL% %DOWNLOAD_PATH%
-bitsadmin.exe /TRANSFER xyzzy-download "%DOWNLOAD_URL%" "%DOWNLOAD_PATH%"
+rem --------------------------------
+rem  インストール
+rem
+rem echo %DOWNLOAD_URL% %DOWNLOAD_PATH%
+rem bitsadmin.exe /TRANSFER xyzzy-download "%DOWNLOAD_URL%" "%DOWNLOAD_PATH%"
 
+rem --------------------------------
+rem  ランチャー追加
+rem
+if not exist "%LAUNCHAR_PATH%" (
+    echo yamyランチャーを作成します
+    cmd /c %BIN%\tools\mksc.bat "%LAUNCHAR_PATH%" "%EXE_PATH%"
+)
 
-
-rem echo yamyをスタートアップに登録する
-rem cmd /c mksc "%STARTUP_FOLDER%\yamy.lnk" "%BIN_FOLDER%\yamy\yamy.exe"
+rem --------------------------------
+rem  スタートアップ追加
+rem
+if not exist "%STARTUP_PATH%" (
+    echo yamyをスタートアップに登録します
+    cmd /c %BIN%\tools\mksc.bat "%STARTUP_PATH%" "%EXE_PATH%"
+)
 
 pause
 
