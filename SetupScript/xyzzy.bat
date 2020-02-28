916d57c83f1aa3041d679668a621f5e8797b046f
matsumoto_tatsuhei <matsumoto_tatsuhei@intec.co.jp>
2020-02-28 10:57:55 +0900

    update xyzzy setting script.

--- SetupScript/xyzzy.bat
+++ SetupScript/xyzzy.bat
@@ -1,17 +1,35 @@
 @echo off
-echo xyzzyのセットアップ
 
+echo --------------------------------
+echo  xyzzyのセットアップ
+echo --------------------------------
+
+rem --------------------------------
+rem  基本設定
+rem
 set BIN=%USERPROFILE%\bin
-set SENDTO=%APPDATA%\Microsoft\Windows\SendTo
+set EXE_PATH1=%USERPROFILE%\bin\xyzzy\xyzzy.exe
+set EXE_PATH2=%USERPROFILE%\bin\xyzzy\xyzzycli.exe
+set LAUNCHAR_PATH1=%USERPROFILE%\bin\xn.lnk
+set LAUNCHAR_PATH2=%USERPROFILE%\bin\x.lnk
+set SENDTO_PATH1=%APPDATA%\Microsoft\Windows\SendTo\xn.lnk
+set SENDTO_PATH2=%APPDATA%\Microsoft\Windows\SendTo\x.lnk
 
-echo xyzzyをランチャーに追加
-cmd /c %BIN%\tools\mksc.bat "%BIN%\x.lnk" "%BIN%\xyzzy\xyzzycli.exe"
-cmd /c %BIN%\tools\mksc.bat "%BIN%\xn.lnk" "%BIN%\xyzzy\xyzzy.exe"
+rem --------------------------------
+rem  xyzzyをランチャーに追加
+rem
+cmd /c %BIN%\tools\mksc.bat "%LAUNCHAR_PATH1%" "%EXE_PATH1%"
+cmd /c %BIN%\tools\mksc.bat "%LAUNCHAR_PATH2%" "%EXE_PATH2%"
 
-echo xyzzyをSendToに登録する
-cmd /c mksc "%SENDTO%\x.lnk" "%BIN%\xyzzy\xyzzycli.exe"
+rem --------------------------------
+rem  xyzzyをSendToに登録する
+rem
+cmd /c %BIN%\tools\mksc.bat "%SENDTO_PATH1%" "%EXE_PATH1%"
+cmd /c %BIN%\tools\mksc.bat "%SENDTO_PATH2%" "%EXE_PATH2%"
 
-echo xyzzy用の環境変数を作成する
+rem --------------------------------
+rem  xyzzy用の環境変数を作成する
+rem
 setx XYZZYHOME %BIN%\xyzzy
 
 pause
